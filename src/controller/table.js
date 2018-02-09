15ff269ecd6d1c90ab25f43f074a1efdd4b09e69
Ezra Stas Babushkin <diablostas@mail.ru>
2018-02-10 01:26:07 +0300

    Paging finished

--- src/controller/table.js
+++ src/controller/table.js
@@ -11,21 +11,17 @@ export default class table {
     this.allData = data;
     this.perPage = perPage;
     this.MainEl = el;
-
-    // this.data = this.allData.slice(0, this.perPage);
-    // console.log(this.data);
-
     this.page = 1;
     this.NumPage = Math.ceil(this.allData.length / this.perPage);
-  }
-
-  init() {
     this.table = document.createElement('table');
     this.thead = document.createElement('thead');
     this.div = document.createElement('div');
     this.tbody = document.createElement('tbody');
     this.tr = document.createElement('tr');
     this.paging = document.createElement('div');
+  }
+
+  init() {
 //создаем структуру таблицы
     this.MainEl.appendChild(this.div);
     this.div.appendChild(this.table);
@@ -37,7 +33,6 @@ export default class table {
 //записываем в неё данные
     this.renderHeader();
     this.renderData();
-
   }
 
   renderHeader(headers = []) {
@@ -51,28 +46,16 @@ export default class table {
     });
   }
 
-  renderData() {
-    this.data = this.allData;
-    this.data = this.data.slice(
-      (this.page - 1) * this.perPage,
-      this.page * this.perPage,
-    );
-
-  if (this.tbody.hasChildNodes()) { // обновление данных на странице
-    console.log(this.tbody.hasChildNodes());
-    let rows = this.tbody.childNodes;
-    for (let i = 0; i < rows.length; i++) {
-      let fields = rows[i].childNodes;
-      let rowKeys = Object.keys(this.data[i]);
-      for (let j = 0; j < fields.length; j++) {
-        fields[j].innerText = this.data[i][rowKeys[j]];
-      }
-    }
-  } else {
-    while (this.tbody.firstChild) {
+  renderData(page) {
+      !!page?this.page = page:''; //проверяем, нажали ли на пагинатор(если нет - то page = undefined).
+        this.data = this.allData;
+        this.data = this.data.slice(
+          (this.page - 1) * this.perPage,
+          this.page * this.perPage,
+        );
+    while (this.tbody.firstChild) {  //убираем предыдущие записи
       this.tbody.removeChild(this.tbody.firstChild);
     }
-console.log(this.tbody.hasChildNodes());
     this.data.forEach(item => {
       let row = document.createElement('tr');
       for (let key in item) {
@@ -82,56 +65,53 @@ console.log(this.tbody.hasChildNodes());
       }
       this.tbody.appendChild(row);
     });
-  }
     this.renderPaging();
-}
+  }
 
   renderPaging(){
-    this.oldPageNum = this.NumPage;
-    this.NumPage = Math.ceil(this.allData.length / this.perPage);
+         this.NumPage = Math.ceil(this.allData.length / this.perPage);
 
-      if (
-              this.oldPageCount === this.pageCount &&
-              this.paging.hasChildNodes()
-            ) {
-              this.paging.firstChild.dataset.page = Math.max(1, this.page - 1);
-              for (let i = 1; i < this.paging.childNodes.length - 1; i++) {
-                this.paging.childNodes[i].dataset.page = i;
-              }
-              this.paging.lastChild.dataset.page = Math.min(
-                this.pageCount,
-                this.page + 1,
-              );
-            } else {
-      while (this.paging.firstChild) {
-  this.paging.removeChild(this.paging.firstChild);
-}
-    console.log(this.NumPage);
+         let previous = document.createElement('span');
+         previous.innerText = 'Previous';
 
-for (let i = 0; i <= this.NumPage; i++) {
-  let page = document.createElement('span');
-  page.innerText = i.toString();
-  page.dataset.page = i;
-  if(i===0){page.innerText ="previous";}
-  this.paging.appendChild(page);
-  console.log(page,page.dataset.page,page.innerText);
-}
+         let next = document.createElement('span');
+         next.innerText = 'Next';
+         console.log(this.paging.hasChildNodes());
+          if (this.paging.hasChildNodes()) {
+                  this.paging.firstChild.dataset.page = Math.max(1, this.page - 1);
+                  for (let i = 1; i < this.paging.childNodes.length - 1; i++) {
+                    this.paging.childNodes[i].dataset.page = i;
+                  }
+                  this.paging.lastChild.dataset.page = Math.min(
+                    this.NumPage,
+                    this.page + 1,
+                  );
+                } else {
+          while (this.paging.firstChild) {
+      this.paging.removeChild(this.paging.firstChild);
+    }
 
-this.paging.childNodes.forEach(item => {
-  item.onclick = () => {
-    this.changePage(parseInt(item.dataset.page));
+    for (let i = 0; i <= this.NumPage+1; i++) {
+      if(i==0){
+        this.paging.appendChild(previous);
+      }else{
+          if (i!=(this.NumPage+1)){
+            let page = document.createElement('span');
+            page.innerText = i.toString();
+            page.dataset.page = i;
+            this.paging.appendChild(page);
+          }else{
+            next.dataset.page = Math.min(this.NumPage, this.page + 1);
+            this.paging.appendChild(next);
+          }
+      }
+    }
+    this.paging.childNodes.forEach(item => {
+      item.onclick = () => {
+        this.renderData(parseInt(item.dataset.page));
+      };
+    });
 
-    console.log("dataset",item.dataset.page);//NaN
-  };
-});
-
-}
+    }
   }
-  changePage(page) {
-  if (this.page !== page) {
-    this.page = page;
-    console.log(this.page);
-    this.renderData();
-  }
-}
 };
