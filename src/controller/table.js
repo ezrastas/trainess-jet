187e23fe000dce3310b35759a8d2097a335f7a6d
Ezra Stas Babushkin <diablostas@mail.ru>
2018-02-11 02:26:08 +0300

    Made a search and some updates

--- src/controller/table.js
+++ src/controller/table.js
@@ -3,11 +3,10 @@
 export default class table {
   constructor({
     el = document.createElement('div'),
-    headers = [],
     data = [],
     perPage = 5,
   }) {
-    this.headers = headers;
+    this.headers = Object.keys(data[0]);
     this.allData = data;
     this.perPage = perPage;
     this.MainEl = el;
@@ -19,43 +18,98 @@ export default class table {
     this.tbody = document.createElement('tbody');
     this.tr = document.createElement('tr');
     this.paging = document.createElement('div');
+    //this.search = document.createElement('div');
   }
 
   init() {
+    //создаём поле поиска
+
+    let search = document.createElement('div');
+
+    let searchTxt = document.createElement('span');
+    searchTxt.innerText = 'Search:';
+    search.appendChild(searchTxt);
+
+    this.searchInput = document.createElement('input');
+
+    let searchBtn = document.createElement('button');
+    searchBtn.innerText = 'Search';
+    searchBtn.onclick = () => {
+      this.SearchRequest(this.searchInput.value);
+    };
+
+    search.appendChild(this.searchInput);
+    search.appendChild(searchBtn);
+    this.div.appendChild(search);
+
 //создаем структуру таблицы
-    this.MainEl.appendChild(this.div);
-    this.div.appendChild(this.table);
+
+     this.MainEl.appendChild(this.div);
+     this.div.appendChild(this.table);
+
+    //this.MainEl.appendChild(this.table);
+
     this.table.appendChild(this.thead);
     this.thead.appendChild(this.tr);
     this.table.appendChild(this.tbody);
 
     this.div.appendChild(this.paging);
 //записываем в неё данные
-    this.renderHeader();
+    this.renderHeader(this.headers);
     this.renderData();
   }
 
-  renderHeader(headers = []) {
-    if (headers.length > 0) {
-      this.headers = headers;
-    }
+  SearchRequest(request = '') {
+    this.searchRec = request;
+    this.page = 1;
+    this.renderData();
+  }
+
+  SortTable(){
+    console.log("test");
+    // 
+    // this.renderData();
+
+  }
+
+  renderHeader() {
     this.headers.forEach(item => {
       let td = document.createElement('td');
-      td.innerText = item.title;
+      item = item.replace("_"," ");
+      item = item[0].toUpperCase()+ item.slice(1);
+      td.innerText = item;
       this.tr.appendChild(td);
+      td.onclick = () => {
+        this.SortTable();
+      };
     });
   }
 
   renderData(page) {
-      !!page?this.page = page:''; //проверяем, нажали ли на пагинатор(если нет - то page = undefined).
-        this.data = this.allData;
+      // console.log(page);
+       !!page?this.page = page:''; //проверяем, нажали ли на пагинатор(если нет - то page = undefined).
+
+       this.data = this.allData;
+if (!!this.searchRec) {
+  this.data = this.allData.filter(item => {
+    for (let key in item) {
+      if (item[key].toString().includes(this.searchRec)) {
+        return true;
+      }
+    }
+    return false;
+  });
+}
+
+this.searchData = this.data;
         this.data = this.data.slice(
           (this.page - 1) * this.perPage,
           this.page * this.perPage,
-        );
-    while (this.tbody.firstChild) {  //убираем предыдущие записи
+        );//возвращает записи для каждой из страниц
+    while (this.tbody.firstChild) {  //очищает предыдущие записи
       this.tbody.removeChild(this.tbody.firstChild);
     }
+
     this.data.forEach(item => {
       let row = document.createElement('tr');
       for (let key in item) {
@@ -69,49 +123,37 @@ export default class table {
   }
 
   renderPaging(){
-         this.NumPage = Math.ceil(this.allData.length / this.perPage);
 
          let previous = document.createElement('span');
          previous.innerText = 'Previous';
 
          let next = document.createElement('span');
          next.innerText = 'Next';
-         console.log(this.paging.hasChildNodes());
-          if (this.paging.hasChildNodes()) {
-                  this.paging.firstChild.dataset.page = Math.max(1, this.page - 1);
-                  for (let i = 1; i < this.paging.childNodes.length - 1; i++) {
-                    this.paging.childNodes[i].dataset.page = i;
-                  }
-                  this.paging.lastChild.dataset.page = Math.min(
-                    this.NumPage,
-                    this.page + 1,
-                  );
-                } else {
-          while (this.paging.firstChild) {
-      this.paging.removeChild(this.paging.firstChild);
-    }
 
-    for (let i = 0; i <= this.NumPage+1; i++) {
-      if(i==0){
-        this.paging.appendChild(previous);
-      }else{
-          if (i!=(this.NumPage+1)){
+          if (this.paging.hasChildNodes()) {
+            while (this.paging.firstChild) { //очистка предыдущего пейджинга
+
+        this.NumPage = Math.ceil(this.searchData.length / this.perPage);
+        this.paging.removeChild(this.paging.firstChild);
+      }}
+
+      previous.dataset.page = Math.min(this.NumPage, this.page - 1);
+      this.paging.appendChild(previous);
+
+      for (let i = 1; i <= this.NumPage; i++) {
             let page = document.createElement('span');
             page.innerText = i.toString();
             page.dataset.page = i;
             this.paging.appendChild(page);
-          }else{
-            next.dataset.page = Math.min(this.NumPage, this.page + 1);
-            this.paging.appendChild(next);
-          }
       }
-    }
+
+      next.dataset.page = Math.min(this.NumPage, this.page + 1);
+      this.paging.appendChild(next);
+
     this.paging.childNodes.forEach(item => {
       item.onclick = () => {
         this.renderData(parseInt(item.dataset.page));
       };
     });
-
-    }
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import {data} from './data.js';
 import table from './controller/table.js';
 
-let headers = [
-  {
-    title: 'Name',
-    type: 'String',
-  },
-  {
-    title: 'Position',
-    type: 'String',
-  },
-  {
-    title: 'Office',
-    type: 'String',
-  },
-  {
-    title: 'Age',
-    type: 'Number',
-  },
-  {
-    title: 'Start date',
-    type: 'Date',
-  },
-  {
-    title: 'Salary',
-    type: 'String',
-  },
-];
-
 const tbl = new table({
   el: document.getElementById('tbl'),
-  headers: headers,
   data: data,
   perPage: 10,
 });
